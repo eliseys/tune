bfee9ae82d12023404d7dab499e6c285fa2a8f53
eliseys <kolesnikovkda@gmail.com>
2023-08-25 12:34:08 +0400

    installation script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+
+sudo apt-get install \
+     libcairo2-dev libnetpbm10-dev netpbm \
+     libpng-dev libjpeg-dev python-numpy \
+     python-pyfits python-dev zlib1g-dev \
+     libbz2-dev swig libcfitsio-dev
+
+
+wget http://astrometry.net/downloads/astrometry.net-latest.tar.gz
+tar xvzf astrometry.net-latest.tar.gz
+cd astrometry.net-*
+
+
+
+# TBD
+
